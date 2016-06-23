d0cabd7ce64237b84414449086fa42810ae01a92
Prashant V <prashant@prashantv.com>
2016-06-22 22:26:15 -0700

    Add more examples

--- protectmem_example_test.go
+++ protectmem_example_test.go
@@ -1,12 +1,14 @@
 package protectmem_test
 
 import (
+	"fmt"
+	"net"
 	"unsafe"
 
 	"github.com/prashantv/protectmem"
 )
 
-func Example() {
+func Example_allocateAndProtect() {
 	type S struct {
 		F1 string
 		F2 string
@@ -33,5 +35,79 @@ func Example() {
 	sAlloc.Protect(protectmem.None)
 
 	// Even reads will fail.
-	_ = sPtr.F1
+	fmt.Println(sPtr.F1)
+}
+
+func Example_allocateSliceAndProtect() {
+	var ints []int
+
+	// Initializes ints to be a slice with 10 capacity, 0 length.
+	alloc := protectmem.AllocateSlice(&ints, 10)
+
+	for i := 0; i < cap(ints); i++ {
+		ints = append(ints, i)
+	}
+
+	// Make the slice read only, can still print the slice fine.
+	alloc.Protect(protectmem.Read)
+	fmt.Println(ints)
+
+	// ints cannot be written to, the following would crash
+	ints[1] = 5
+
+	// We can also protect the slice from any reads or writes
+	alloc.Protect(protectmem.None)
+
+	// Now even a read will fail
+	fmt.Println(ints)
+}
+
+func ExampleAllocate() {
+	ipAlloc := protectmem.Allocate(unsafe.Sizeof(net.IPAddr{}))
+	ip := (*net.IPAddr)(ipAlloc.Ptr())
+
+	_, _ = ipAlloc, ip
+}
+
+func ExampleAllocateSlice() {
+	var strings []string
+	alloc := protectmem.AllocateSlice(&strings, 100)
+
+	_ = alloc
+
+	fmt.Println("length", len(strings), "cap", cap(strings))
+	// Output:
+	// length 0 cap 100
+}
+
+func ExampleAllocation_Access() {
+	alloc := protectmem.Allocate(unsafe.Sizeof(net.IPAddr{}))
+
+	// Access returns the permissions for the alloc.
+	// By default, everything starts off with read + write.
+	fmt.Println(alloc.Access())
+
+	// Output:
+	// read+write
+}
+
+func ExampleAllocation_Protect() {
+	ipAlloc := protectmem.Allocate(unsafe.Sizeof(net.IPAddr{}))
+	ip := (*net.IPAddr)(ipAlloc.Ptr())
+
+	// Protect can be used to change the access permissions for the alloc.
+	ipAlloc.Protect(protectmem.Read)
+
+	// ip cannot be written to, any writes will crash.
+	ip.Zone = "this will fail"
+}
+
+func ExampleAllocation_Ptr() {
+	ipAlloc := protectmem.Allocate(unsafe.Sizeof(net.IPAddr{}))
+
+	// Ptr returns an unsafe.Pointer which can be casted into a pointer
+	// of the type specified in Allocate.
+	ip := (*net.IP)(ipAlloc.Ptr())
+
+	_, _ = ipAlloc, ip
 }
